653dc85fab3cd40fcad0a784d040f4ca982da043
Amber Jones <amber.311.jones@gmail.com>
2025-05-20 09:24:24 -0500

    master

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>HOME</h1>
+    <h1>amber</h1>
+    {{ route("/test") }}
+
+    <form action="/{{route("/formsubmitted")}}" method="POST">
+        @csrf
+        <label for="fullname">Full Name:</label>
+        <input type="text"
+                name="fullname"
+                id="fullname"
+                placeholder="Type your full name"
+                required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="text"
+               id="email"
+               placeholder="Type your email"
+               required>
+        <br>
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,52 @@
 
 use Illuminate\Support\Facades\Route;
 
+// main
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/', function () {
-    return view('contact');
+// URI Parameters
+Route::get('/portfolio',
+ function () {
+    // return $firstname;
+    return view('portfolio');
 });
+Route::get('/portfolio/{firstname}/{lastname}',
+ function ($firstname, $lastname) {
+    // return $firstname;
+    return view('portfolio');
+});
+
+// named route
+Route::get('/test', function () {
+    // return "this is a test";
+    return view('contact');
+})->name("/test");
+
+// SUB ROUTES
+Route::prefix("/home")->group(function () {  
+    Route::get("/experience", function () {
+        // return "experience";
+        return view("experience");
+     });
+
+     Route::get("/education", function () {
+        // return "education";
+        return view("education");
+     });
+});
+
+// POST route
+Route::post("/formsubmitted",
+ function(Request $request) {
+    //! VALIDATION
+    $request->validate([
+        "fullname" => "required|min:3|max:30",
+        "email"=> "required|min:3|max:30|email",
+    ]);
+
+    $full_name = $request->input("fullname");
+    $email = $request->input("email");
+    return "form";
+})->name("/formsubmitted");
